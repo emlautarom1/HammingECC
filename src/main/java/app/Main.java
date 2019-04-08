b2abf345484459d739a2702ad2f41f808d8bfc06
lautaroem1 <emlautarom1@gmail.com>
2019-04-08 00:00:53 -0300

    Added time stamps prints

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,9 +5,13 @@ import app.services.Hamming;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Program start");
+        long startTime = System.nanoTime();
+
         int hammingLevel = 7;
         int chunkSize = 4;
 
@@ -17,7 +21,10 @@ public class Main {
 
             byte[] outputBytes = Hamming.encode(data, hammingLevel, chunkSize);
 
-            Files.write(Paths.get("encoded.txt"), outputBytes);
+            Files.write(Paths.get("encoded.bin"), outputBytes);
+
+            long endTime = System.nanoTime();
+            System.out.println("Program finished succesfully in " + (endTime - startTime) / 1000000 + " milliseconds");
 
         } catch (Exception e) {
             e.printStackTrace();
